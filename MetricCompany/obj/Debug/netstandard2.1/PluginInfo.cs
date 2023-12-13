6cde78a1a6ab189e7b64f2c51e3a6c33e00eaf2b
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-12-13 02:10:40 +0000

    Rename everything to MetricCompany

---
+++ MetricCompany/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace MetricCompany
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "MetricCompany";
+        public const string PLUGIN_NAME = "MetricCompany";
+        public const string PLUGIN_VERSION = "0.1.0";
+    }
+}
